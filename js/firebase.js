4c4da5d6391b39cf68086d494481982d82976170
Rosivaldo <rosivaldo.correia@terra.com.br>
2025-06-15 13:38:03 -0300

    Atulizando o gitgnore

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDWELBWgBlzQ01rAVDDhzeIFDzjYbqr6Jw",
+  authDomain: "webtrack-8ff15.firebaseapp.com",
+  projectId: "webtrack-8ff15",
+  storageBucket: "webtrack-8ff15.firebasestorage.app",
+  messagingSenderId: "868095324397",
+  appId: "1:868095324397:web:608fea4a13443b20d6dce4",
+  databaseURL: "https://webtrack-8ff15-default-rtdb.firebaseio.com/"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
